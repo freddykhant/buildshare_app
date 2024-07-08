849296d69eb095a0121e09df3dfba68373699fb5
Freddy Khant <105434008+freddykhant@users.noreply.github.com>
2024-07-08 20:20:09 +0800

    sign in form created, database functionality not yet

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const SignupValidation = z.object({
+  name: z
+    .string()
+    .min(2, { message: "Name must be at least 2 characters long" }), // 👈 Add a custom error message.
+  username: z
+    .string()
+    .min(2, { message: "Username must be at least 2 characters long" })
+    .max(50),
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" }),
+});
